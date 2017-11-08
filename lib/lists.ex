aa772b47123e8ff39ca1388ef6d855040b21d696
Sujay Mansingh <sujay.mansingh@gmail.com>
2017-11-08 11:05:58 +0000

    Add function to find penultimate item in list

--- lib/lists.ex
+++ lib/lists.ex
@@ -11,4 +11,13 @@ defmodule Lists do
   """
   def last([element]), do: element
   def last([_ | t]), do: last(t)
+
+  @doc """
+  Find the last but one element in a list.
+
+    iex> Lists.last_but_one([:a, :b, :c, :d])
+    :c
+  """
+  def last_but_one([penultimate, _ultimate]), do: penultimate
+  def last_but_one([_ | t]), do: last_but_one(t)
 end
