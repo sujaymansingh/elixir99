1e4cde9677ec927878705ed55a44d547331df502
Sujay Mansingh <sujay.mansingh@gmail.com>
2017-11-15 21:47:55 +0000

    Add func to find all prime numbers in range

--- lib/arithmetic.ex
+++ lib/arithmetic.ex
@@ -76,4 +76,16 @@ defmodule Arithmetic do
     |> Lists.encode()
     |> Enum.map(fn {a, b} -> {b, a} end)
   end
+
+  @doc """
+  A list of prime numbers.
+
+  Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+
+    iex> Arithmetic.prime_numbers_in_range(1, 20)
+    [1, 2, 3, 5, 7, 11, 13, 17, 19]
+  """
+  def prime_numbers_in_range(n1, n2) do
+    Lists.range(n1, n2) |> Enum.filter(&prime?/1)
+  end
 end
