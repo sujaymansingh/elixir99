9cb347b17c3f1af76945dac313ca09a430c64e28
Sujay Mansingh <sujay.mansingh@gmail.com>
2017-12-06 23:07:34 +0000

    Add function to get all possible balanced trees

--- lib/binary_tree.ex
+++ lib/binary_tree.ex
@@ -20,4 +20,45 @@ defmodule BinaryTree do
   def istree?(nil), do: true
   def istree?({_, left, right}), do: istree?(left) and istree?(right)
   def istree?(_), do: false
+
+  @doc """
+  Construct completely balanced binary trees
+
+  In a completely balanced binary tree, the following property holds for every node:
+  The number of nodes in its left subtree and the number of nodes in its right subtree are almost 
+  equal, which means their difference is not greater than one.
+  """
+  def cbal_tree(0, _), do: [nil]
+
+  def cbal_tree(n, default_element) do
+    num_remaining = n - 1
+
+    case rem(num_remaining, 2) do
+      0 ->
+        # This is easy, as each side can only have num_remaining/2 nodes
+        each_side = div(num_remaining, 2)
+        possible_subtrees = cbal_tree(each_side, default_element)
+        build_possible_trees(default_element, possible_subtrees, possible_subtrees)
+
+      1 ->
+        # This is a bit more complex. The number of nodes on each side can differ by only 1.
+        # n = m + (m + 1)
+        # Thus we have two options:
+        # 1) left has m, right has m + 1
+        # 2) left has m + 1, right has m
+        m = div(num_remaining, 2)
+        little_half = cbal_tree(m, default_element)
+        bigger_half = cbal_tree(m + 1, default_element)
+
+        build_possible_trees(default_element, little_half, bigger_half) ++
+          build_possible_trees(default_element, bigger_half, little_half)
+    end
+  end
+
+  defp build_possible_trees(default_element, possible_left_subtrees, possible_right_subtrees) do
+    for possible_left <- possible_left_subtrees,
+        possible_right <- possible_right_subtrees do
+      {default_element, possible_left, possible_right}
+    end
+  end
 end
--- test/binary_tree_test.exs
+++ test/binary_tree_test.exs
@@ -1,4 +1,33 @@
 defmodule BinaryTreeTest do
   use ExUnit.Case, async: true
   doctest BinaryTree
+
+  test "all balances trees with 2 nodes" do
+    # This is simple enough... the 2nd node is either on the left or the right.
+    expected = [
+      {:x, {:x, nil, nil}, nil},
+      {:x, nil, {:x, nil, nil}}
+    ]
+
+    actual = BinaryTree.cbal_tree(2, :x)
+    assert length(actual) == length(expected)
+    assert MapSet.new(actual) == MapSet.new(expected)
+  end
+
+  test "all balances trees with 4 nodes" do
+    # One way to think about this is:
+    # If we had three nodes, we'd have
+    # {:x, {:x, nil, nil}, {:x, nil, nil}}
+    # So the final node, could replace any of the nils: thus 4 options
+    expected = [
+      {:x, {:x, {:x, nil, nil}, nil}, {:x, nil, nil}},
+      {:x, {:x, nil, {:x, nil, nil}}, {:x, nil, nil}},
+      {:x, {:x, nil, nil}, {:x, {:x, nil, nil}, nil}},
+      {:x, {:x, nil, nil}, {:x, nil, {:x, nil, nil}}}
+    ]
+
+    actual = BinaryTree.cbal_tree(4, :x)
+    assert length(actual) == length(expected)
+    assert MapSet.new(actual) == MapSet.new(expected)
+  end
 end
