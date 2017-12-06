683a34ef4c45f9016d65eaadcbd734911d9ac6ab
Sujay Mansingh <sujay.mansingh@gmail.com>
2017-12-06 21:04:37 +0000

    Add simple truth table function
    The actual prolog problem seems to require an expression to be passed
    in. I suspect this works on prolog due to some variable 'binding' rules.
    
    To do this in Elixir, I might have to use a macro. It doesn't seem worth
    doing that at this point.

--- lib/logic.ex
+++ lib/logic.ex
@@ -1,2 +1,34 @@
 defmodule Logic do
+  @moduledoc """
+  See https://sites.google.com/site/prologsite/prolog-problems/3
+  """
+  def and_(a, b), do: a and b
+
+  def or_(a, b), do: a or b
+
+  def nand(a, b), do: not(a and b)
+
+  def nor(a, b), do: not(a or b)
+
+  def xor(false, true), do: true
+  def xor(true, false), do: true
+  def xor(_, _), do: false
+
+  @doc """
+  Produce a truth table for the given number of inputs.
+
+  n is the number of inputs
+  f is a function that takes a list of size n and returns true or false
+
+  This is slightly different to the way the prolog example works.
+  There, it uses the concept of binding in prolog. I don't believe this will work in
+  elixir, without some invasive macros.
+
+  Thus, for now, I'm writing a truth table function.
+  """
+  def table(n, f) do
+    Enum.map(1..n, fn _ -> [true, false] end)
+    |> Lists.cartesian_product()
+    |> Enum.map(fn inputs -> inputs ++ [f.(inputs)] end)
+  end
 end
--- test/logic_test.exs
+++ test/logic_test.exs
@@ -1,4 +1,16 @@
 defmodule LogicTest do
   use ExUnit.Case, async: true
   doctest Logic
+
+  test "producing a truth table" do
+    f = fn [a, b] -> Logic.and_(a, Logic.or_(a, b)) end
+    result = Logic.table(2, f)
+
+    assert result == [
+             [true, true, true],
+             [true, false, true],
+             [false, true, false],
+             [false, false, false]
+           ]
+  end
 end
